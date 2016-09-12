1bfcb49393dc0a11e7ff98927afdc51b608cddc0
liang22 <seed.liang@gmail.com>
2016-09-13 02:16:57 +0800

    Add files via upload

---
+++ ch01/0113.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    int sum=0;
+
+    for(int i=50;i<=100;++i)
+        sum=sum+i;
+    std::cout<<sum<<std::endl;
+
+    for(int j=10;j>=0;--j)
+        std::cout<<j<<std::endl;
+}
