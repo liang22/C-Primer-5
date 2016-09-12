a72234202eb8e5a534a69c8d49b1984e153cd8dd
liang22 <seed.liang@gmail.com>
2016-09-13 01:24:34 +0800

    Add files via upload

---
+++ ch01/0105.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+  int i,j=0;
+  std::cout<<"Put in the two numbers!"<<std::endl;
+  std::cin>>i>>j;
+  std::cout<<i;
+  std::cout<<"*";
+  std::cout<<j;
+  std::cout<<"=";
+  std::cout<<i*j;
+  std::cout<<std::endl;
+  return 0;
+}
---
+++ ch01/0106.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+  int i,j=0;
+  std::cout<<"Put in the two numbers!"<<std::endl;
+  std::cin>>i>>j;
+  std::cout<<"The sum of "<<i
+           <<" and "<<j
+           <<" is "<<i+j<<std::endl;
+  return 0;
+}
